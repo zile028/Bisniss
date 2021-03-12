cdfc2a638d845499cb24971be2a611d4bde9e46f
zile028@gmail.com <zile028@gmail.com>
2021-03-12 23:00:13 +0100

    responsive 767

--- index.php
+++ index.php
@@ -1,5 +1,154 @@
 <?php include "partials/header.php"?>
 
-<h1>Sutra content</h1>
+<header class="mainHeader">
+    <h1>Trusted and <br> professional adviser</h1>
+    <h3>For your buisniess</h3>
+    <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+    <button>About us</button>
+</header>
+<section class="headings container">
+    <article>
+        <h2>01</h2>
+    </article>
+    <article>
+        <h3>About us</h3>
+        <h2><span>we can</span> help business grow</h2>
+        <h4>get your Business in 4 easy steps</h4>
+    </article>
+</section>
+<section class="about">
+    <article><img src="img/about.jpg" alt=""></article>
+    <article>
+        <h2>evrething you need in one solution</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste
+            voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit
+            molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio
+            repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>
+        </p>
+    </article>
+</section>
+<section class="headings container">
+    <article>
+        <h2>02</h2>
+    </article>
+    <article>
+        <h3>Services</h3>
+        <h2><span>how to provide</span> tools that help evryone</h2>
+        <h4>get your Business in 4 easy steps</h4>
+    </article>
+</section>
+<section class="servicies container">
+    <article>
+        <i class="far fa-chart-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam quibusdam ducimus nisi deserunt
+            praesentium laboriosam totam ab, earum laudantium!</p>
+    </article>
 
+    <article>
+        <i class="fas fa-bullseye"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam quibusdam ducimus nisi deserunt
+            praesentium laboriosam totam ab, earum laudantium!</p>
+    </article>
+
+    <article>
+        <i class="fas fa-map-signs"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam quibusdam ducimus nisi deserunt
+            praesentium laboriosam totam ab, earum laudantium!</p>
+    </article>
+
+    <article>
+        <i class="fas fa-filter"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam quibusdam ducimus nisi deserunt
+            praesentium laboriosam totam ab, earum laudantium!</p>
+    </article>
+
+    <article>
+        <i class="fas fa-hand-holding-usd"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam quibusdam ducimus nisi deserunt
+            praesentium laboriosam totam ab, earum laudantium!</p>
+    </article>
+
+    <article>
+        <i class="fas fa-bullhorn"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati aliquam quibusdam ducimus nisi deserunt
+            praesentium laboriosam totam ab, earum laudantium!</p>
+    </article>
+
+</section>
+<section class="business">
+    <header class="headings container">
+        <article>
+            <h2>03</h2>
+        </article>
+        <article>
+            <h3>Services</h3>
+            <h2><span>how to provide</span> tools that help evryone</h2>
+            <h4>get your Business in 4 easy steps</h4>
+            <button>About us</button>
+        </article>
+    </header>
+    <article>
+        <div>
+            <h2>18</h2>
+            <h4>Nominations</h4>
+            <hr>
+        </div>
+        <div>
+            <h2>5</h2>
+            <h4>Awards</h4>
+            <hr>
+        </div>
+        <div>
+            <h2>45</h2>
+            <h4>Agencies</h4>
+            <hr>
+        </div>
+    </article>
+</section>
+
+<section class="headings container">
+    <article>
+        <h2>04</h2>
+    </article>
+    <article>
+        <h3>our team</h3>
+        <h2><span>we have a professional</span> team og business analysts</h2>
+        <h4>get your Business in 4 easy steps</h4>
+    </article>
+</section>
+
+<section class="team container">
+    <article>
+        <img src="img/team1.jpg" alt="">
+        <h4>James Douglas</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi at consequuntur perferendis officiis id,
+            earum alias aliquam voluptates odit libero?</p>
+    </article>
+
+    <article>
+        <img src="img/team2.jpg" alt="">
+        <h4>Peter Green</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi at consequuntur perferendis officiis id,
+            earum alias aliquam voluptates odit libero?</p>
+    </article>
+
+    <article>
+        <img src="img/team3.jpg" alt="">
+        <h4>Etta James</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi at consequuntur perferendis officiis id,
+            earum alias aliquam voluptates odit libero?</p>
+    </article>
+</section>
 <?php include "partials/footer.php"?>
